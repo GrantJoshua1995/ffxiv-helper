24f161c71e325ecc624b87ed928436eb2fdb62f5
Joshua Grant <contact@joshua-grant.com>
2020-10-07 13:16:21 +0100

    Added search component (built up of the text field and button components) and made default story

---
+++ src/components/Search/Search.stories.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Search from ".";
+
+export default {
+  title: "Search",
+};
+
+export const Default = () => <Search />;
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,15 @@
+import { Button, TextField } from "@material-ui/core";
+import React from "react";
+
+const Search = () => {
+  return (
+    <>
+      <TextField />
+      <Button variant="contained" color="primary">
+        Search
+      </Button>
+    </>
+  );
+};
+
+export default Search;
